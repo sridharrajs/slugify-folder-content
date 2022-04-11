6cf5bc7a2584606151a87a802b68e0e3c58b20cd
Sridhar Raj Sampath Kumar <sridhar@aghenam.com>
2022-04-11 12:45:39 +0530

    Update commit.py

--- commit.py
+++ commit.py
@@ -2,32 +2,79 @@ import subprocess
 from datetime import date
 from string import Template
 
-# Getting current branch.
-stdout = subprocess.run(["git", "branch"], check=True, capture_output=True, text=True).stdout
-git_branch_name = stdout.split()[1]
-
-## Adding files
-subprocess.run(["git", "add", "."], check=True, capture_output=True, text=True).stdout
-
-stateged_info = subprocess.run(["git", "diff", "--name-only","--cached"], check=True, capture_output=True, text=True).stdout
-
-total_changes = 0
-for file_name in stateged_info.split('\n'):
-    if len(file_name.strip()) > 0:
-        total_changes = total_changes + 1
-
-## Writing commit message.
-today = date.today()
-commit_msg_file = open('.git/COMMIT_EDITMSG', 'w')
+# Utility script to commit and push
+# the changes within the current folder
+# to the remote repo
 
 
-commit_msg = Template('Committed $change_count changes to $branch_name on $commit_date')
-msg = commit_msg.safe_substitute(branch_name= git_branch_name, commit_date=today, change_count=total_changes)
+def get_current_branch():
+    stdout = subprocess.run(["git", "branch"], check=True,
+                            capture_output=True, text=True).stdout
+    return stdout.split()[1]
 
-commit_msg_file.writelines(msg)
-commit_msg_file.close()
 
-stdout = subprocess.run(["git", "commit", "-F", ".git/COMMIT_EDITMSG"], check=True, capture_output=True, text=True).stdout
-stdout = subprocess.run(["git", "push", "origin", git_branch_name, "-f"], check=True, capture_output=True, text=True).stdout
+def stage_files():
+    subprocess.run(["git", "add", "."], check=True,
+                   capture_output=True, text=True).stdout
 
-print(msg)
+
+def assert_file_count(staged_file_count):
+    if staged_file_count == 0:
+        print('No changes to commit')
+        quit()
+
+
+def compute_staged_file_count():
+    stateged_info = subprocess.run(
+        ["git", "diff", "--name-only", "--cached"], check=True, capture_output=True, text=True).stdout
+
+    total_changes = 0
+    for file_name in stateged_info.split('\n'):
+        if len(file_name.strip()) > 0:
+            total_changes = total_changes + 1
+
+    return total_changes
+
+
+def build_commit_msg(staged_file_count, current_branch_name):
+    today = date.today()
+
+    commit_msg = Template(
+        'Committed $change_count changes to $branch_name on $commit_date')
+    msg = commit_msg.safe_substitute(
+        branch_name=current_branch_name, commit_date=today, change_count=staged_file_count)
+
+    return msg
+
+
+def set_commit_msg(msg):
+    commit_msg_file = open('.git/COMMIT_EDITMSG', 'w')
+
+    commit_msg_file.writelines(msg)
+    commit_msg_file.close()
+
+
+def commit_changes():
+    subprocess.run(["git", "commit", "-F", ".git/COMMIT_EDITMSG"],
+                   check=True, capture_output=True, text=True).stdout
+
+
+
+def push_to_origin(current_branch_name):
+    subprocess.run(["git", "push", "origin", current_branch_name,
+                   "-f"], check=True, capture_output=True, text=True).stdout
+
+
+if __name__ == '__main__':
+    current_branch_name = get_current_branch()
+    stage_files()
+
+    staged_file_count = compute_staged_file_count()
+    assert_file_count(staged_file_count)
+    msg = build_commit_msg(staged_file_count, current_branch_name)
+
+    set_commit_msg(msg)
+    commit_changes()
+    push_to_origin(current_branch_name)
+
+    print(msg)
